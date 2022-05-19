822a6f9032745a84981b04840a4791c42c5f42c9
Jose Luis Perez Lara <a21pelajo1@iesgrancapitan.org>
2022-05-19 09:55:36 +0200

    docs: documentación javadoc

---
+++ src/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.jlpl.eed.examen.OptimizacionYDocumentacion"}];updateSearchResults();
---
+++ src/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.jlpl.eed.examen.OptimizacionYDocumentacion","l":"EdadErroneaException"},{"p":"com.jlpl.eed.examen.OptimizacionYDocumentacion","l":"Mascota"},{"p":"com.jlpl.eed.examen.OptimizacionYDocumentacion","l":"Perro"}];updateSearchResults();
